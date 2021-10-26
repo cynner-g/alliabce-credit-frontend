8930242e869b894c81f40f4b0ebf52235d486a76
Jitendra Kumar <jitendra@jbidigital.co.uk>
2021-10-27 02:14:05 +0530

    layout mapping

--- front-end/pages/forgot-password.js
+++ front-end/pages/forgot-password.js
@@ -1,8 +1,34 @@
+
+import Link from "next/link";
+import { useState } from "react";
 const ForgotPassword = () => {
+    const [userName, setUserName] = useState("");
+
     return (
-        <>
-            <h1>Forgot Password</h1>
-        </>
+
+
+
+
+        <div className=" vertical-center">
+            <div className="container">
+                <h1 className="mb-5 text-center">Forgot Password</h1>
+                <div class="row">
+                    <div className="col"></div>
+                    <div className="col align-self-center">
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Email Address</label>
+                            <input className="form-control" type="text" id="username" placeholder="User Name" value={userName} onChange={(e) => setName(e.target.value)} />
+                        </div>
+                        <div className="mb-3">
+                            <button type="submit" className="btn btn-primary mb-3">Continue</button>
+                        </div>
+                        <Link href="/"><a>Login</a></Link>
+                    </div>
+                    <div className="col"></div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -5,20 +5,28 @@ export default function Login() {
   const [userName, setName] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div>
-      <div>
-        <input type="text" id="username" placeholder="User Name" value={userName} onChange={(e) => setName(e.target.value)} />
-        <label htmlFor="username" />
+    <div className=" vertical-center">
+      <div className="container">
+        <h1 className="mb-5 text-center">Log In</h1>
+        <div class="row">
+          <div className="col"></div>
+          <div className="col align-self-center">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">Email Address</label>
+              <input className="form-control" type="text" id="username" placeholder="User Name" value={userName} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password">Password</label>
+              <input className="form-control" type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="mb-3">
+              <button type="submit" className="btn btn-primary mb-3">Login</button>
+            </div>
+            <Link href="/forgot-password"><a>Forgot Password</a></Link>
+          </div>
+          <div className="col"></div>
+        </div>
       </div>
-      <div>
-        <input type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <label htmlFor="password" />
-      </div>
-      <div>
-        <button type="submit">Login</button>
-      </div>
-      <Link href="/signup"><a>Signup</a></Link>
-      <Link href="/forgot-password"><a>Forgot Password</a></Link>
     </div>
   )
 }
