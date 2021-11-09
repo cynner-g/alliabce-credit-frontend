368028b7515a0f20856ceaa32e6916dd5f74f382
Jitendra Kumar <jitendra@jbidigital.co.uk>
2021-11-09 07:40:41 +0530

    cookie functionality

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -2,43 +2,90 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/dist/client/router';
-
+import Cookies from 'js-cookie'
 
 import langTrans from './../components/i18n';
 import Lang from '../components/lang';
+// import gql from 'graphql-tag';
+// import client from './apollo-client';
 
 
+// export const GET_USERS = gql`
+//   query getUsers {
+//     users @rest(type: "Users", path: "users/login") {
+//       data
+//     }
+// }`;
 
 export default function Login(props) {
   const [userName, setName] = useState("");
   const [password, setPassword] = useState("");
-  const { locale, locales, defaultLocale, asPath } = useRouter();
+  const [loginMessage, setloginMessage] = useState("");
+  const { locale, locales, defaultLocale, asPath, router } = useRouter();
   const { login, a_forgot_pass, btn_login } = langTrans[locale];
+
+
+
+  const userLogin = async (e) => {
+
+    e.preventDefault();
+    // const { data, Loading, NetworkStatus, Error } = await client.query({
+    //   query: GET_USERS,
+    // });
+
+    // console.log(data);
+    const res = await fetch(`http://dev.alliancecredit.ca/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3000"
+      },
+      body: JSON.stringify({
+        "language": locale,
+        "email_id": userName,
+        password
+      })
+
+    })
+    const res2 = await res.json();
+    console.log(res2);
+    if (res2.status_code == 403) {
+      setloginMessage("Either User name or Password doesnot exists");
+    } else if (res2.status_code == 200) {
+      setloginMessage("");
+      Cookies.set('token', res2?.data?.auth_token)
+    } else {
+      setloginMessage("Ajax fails");
+    }
+  }
 
   return (
     <div className=" vertical-center">
       <div className="container">
         <Lang />
         <h1 className="mb-5 text-center">{login.title}</h1>
-        <div className="row">
-          <div className="col"></div>
-          <div className="col align-self-center">
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">{login.email}</label>
-              <input className="form-control" type="text" id="username" placeholder="User Name" value={userName} onChange={(e) => setName(e.target.value)} />
+        <form onSubmit={(e) => userLogin(e)}>
+          <div className="row">
+            <div className="col"></div>
+            <div className="col align-self-center">
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">{login.email}</label>
+                <input className="form-control" type="text" id="username" placeholder="User Name" value={userName} onChange={(e) => setName(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password">{login.password}</label>
+                <input className="form-control" type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <button type="submit" className="btn btn-primary mb-3">{btn_login}</button>
+              </div>
+              <Link href="/forgot-password"><a>{a_forgot_pass}</a></Link>
+              {loginMessage}
             </div>
-            <div className="mb-3">
-              <label htmlFor="password">{login.password}</label>
-              <input className="form-control" type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div className="mb-3">
-              <button type="submit" className="btn btn-primary mb-3">{btn_login}</button>
-            </div>
-            <Link href="/forgot-password"><a>{a_forgot_pass}</a></Link>
+            <div className="col"></div>
           </div>
-          <div className="col"></div>
-        </div>
+        </form>
       </div>
-    </div>
+    </div >
   )
 }
