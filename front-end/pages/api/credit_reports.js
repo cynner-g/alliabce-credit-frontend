ce04892535987a9336b949fd77fa874abf126e4f
MetRepoViewer <gsutton@qwantech.com>
2021-11-24 07:28:13 -0500

    Finished renaming files and api call functions

---
+++ front-end/pages/api/credit_reports.js
@@ -0,0 +1,136 @@
+const init = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'no-cors'
+    },
+    body: {
+        api_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzM1MTVhMzFmN2YyYjUwNGI4N2Y1MyIsImVtYWlsX2lkIjoiaml0ZW5kcmFAamtsYWJzLmNhIiwiY3JlYXRlX2RhdGUiOiIyMDIxLTExLTE0VDE5OjI5OjMyLjQwMVoiLCJpYXQiOjE2MzY5MTgxNzJ9.vSVFsenePK75v8XAUbBUPZIxCqAc8rh7rSjnIDgKo44"
+    }
+}
+
+export const order_list = (search, filter) => {
+    init.method = "POST"
+    init.body = JSON.stringify(init.body);
+
+    var myReq = new Request(`${process.env.API_URL}/report/list-order`, init);
+    console.log('fetching');
+    let ret = fetch(myReq)
+        .then((response) => {
+            console.log("Response: ", response)
+            if (response.ok) {
+                return response;
+            }
+            else {
+                var error = new Error("Error " + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, (error) => {
+            var err = new Error(error.message);
+            throw err;
+        })
+        .then((response) => { return response.json() })
+        .then((data) => {
+            return data.data
+        })
+        .catch((err) => {
+
+        })
+    return ret;
+}
+
+export const cancel_order = (rptId) => {
+    if (!rptId) return;
+    let body;
+    try {
+        body = JSON.parse(init.body);
+    }
+    catch (ex) {
+        body = init.body;
+    }
+
+    body.order_id = rptId;
+    init.method = "POST"
+    init.body = JSON.stringify(body);
+
+    var myReq = new Request(`${process.env.API_URL}/report/cancel-order`, init);
+    console.log('fetching');
+    let ret = fetch(myReq)
+        .then((response) => {
+            console.log("Response: ", response)
+            if (response.ok) {
+                return response;
+            }
+            else {
+                var error = new Error("Error " + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, (error) => {
+            var err = new Error(error.message);
+            throw err;
+        })
+        .then((response) => { return response.json() })
+        .then((data) => {
+            return data.data
+
+        })
+        .catch((err) => {
+
+        })
+    return ret;
+}
+
+export const order_details = (rptId) => {
+
+    if (!rptId) return;
+    let body;
+    try {
+        body = JSON.parse(init.body);
+    }
+    catch (ex) {
+        body = init.body;
+    }
+
+    body.order_id = rptId;
+    init.method = "POST"
+    init.body = JSON.stringify(body);
+
+    var myReq = new Request(`${process.env.API_URL}/report/order-details`, init);
+    let ret = fetch(myReq)
+        .then((response) => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                var error = new Error("Error " + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, (error) => {
+            var err = new Error(error.message);
+            throw err;
+        })
+        .then((response) => { return response.json() })
+        .then((data) => {
+            return data.data
+        })
+        .catch((err) => {
+
+        })
+    return ret;
+
+}
+
+export const resubmit_report = (rptData => {
+
+})
+
+export const quick_order_report = (rptData => {
+
+})
+
+export const create_link_token = (rptData => {
+
+})
+
